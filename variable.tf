71436249368f4680abab67d6ee356f5b6d683ea9
rake-learn <abburirakesh@gmail.com>
2024-11-12 20:58:48 -0500

    updated issues

--- variable.tf
+++ variable.tf
@@ -1,7 +1,9 @@
 variable "ecs_cluster_name" {
-  default = "my-learn-ecs-cluster"
+    type    = string
+    default = "my-learn-ecs-cluster"
 }
 
 variable "ecs_service_name" {
-  default = "my-learn-ecs-service"
+    type    = string
+    default = "my-learn-ecs-service"
 }
